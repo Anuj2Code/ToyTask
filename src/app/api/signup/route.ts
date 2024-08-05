693069d6005a647c7d6cbbe9d3fa7a83542523cc
Anuj pal <148964497+Anuj2Code@users.noreply.github.com>
2024-08-05 13:59:45 +0530

    Update route.ts

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -39,7 +39,7 @@ export async function POST(req: NextRequest) {
     } catch (error: any) {
         return NextResponse.json({error:error.message},{status:500})
     }
-    // finally {
-    //     await prisma.$disconnect();
-    // }
+    finally {
+        await prisma.$disconnect();
+    }
 }
